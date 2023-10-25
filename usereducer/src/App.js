0c903f6efb6213a2c88499051a4696912f2ea074
Alex Nahayo <alexn794@gmail.com>
2023-10-25 07:47:27 +0100

    completed mini usereducer project

---
+++ usereducer/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+import BucketListWithReducer from './BucketListWithReducer'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <BucketListWithReducer />
+    </div>
+  )
+}
+
+export default App
